a8222db54333d2805d7ab4ab5ee57f669123b1c9
Kitsunori <kix@HarukaX.localdomain>
2026-03-30 14:17:59 +0000

    Add Mihomo service

--- kix.sh
+++ kix.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ "$EUID" -ne 0 ]; then
     echo "Sudo is required for replacing nix config."
@@ -12,29 +12,114 @@ CONF_DIR="/etc/nixos"
 CONF_MAIN="$CONF_DIR/configuration.nix"
 CONF_BASE="$CONF_DIR/base.nix"
 CONF_VSC="$CONF_DIR/vscode.nix"
+CONF_MHX="$CONF_DIR/mihomo.nix"
 CONF_KIX="$CONF_DIR/kits.nix"
 CONF_VSC_URL="https://raw.githubusercontent.com/K900/vscode-remote-workaround/main/vscode.nix"
-KIX_USER='kix'
-KIX_HOSTNAME='HarukaX'
+KIX_USER="kix"
+KIX_HOSTNAME="HarukaX"
 
 if [ ! -f $CONF_BASE ]; then
   mv $CONF_MAIN $CONF_BASE
-  cat <<CFM> $CONF_MAIN
+fi
+cat <<CFM> "$CONF_MAIN"
 { config, ... }:
 
 {
-  imports = 
+  imports =
   [
     "$CONF_BASE"
     "$CONF_VSC"
+    "$CONF_MHX"
     "$CONF_KIX"
   ];
 }
 CFM
-fi
 
-curl -Lo "$CONF_VSC" "$CONF_VSC_URL"
+curl -sLo "$CONF_VSC" "$CONF_VSC_URL"
 sed -i 's/nodejs-\([0-9]\+\)_x/nodejs_latest/g' "$CONF_VSC"
+
+cat <<'CFH'> "$CONF_MHX"
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.mihox;
+  configDir = "/var/lib/mihomo";
+  configFile = "${configDir}/config.yaml";
+in
+{
+  options.services.mihox =
+  {
+    enable = lib.mkEnableOption "Subscription auto updater for mihomo";
+
+    url = lib.mkOption
+    {
+      type = lib.types.str;
+      description = "Mihomo subscripition";
+      example = "your.provider/identifer";
+    };
+
+    interval = lib.mkOption
+    {
+      type = lib.types.str;
+      default = "weekly";
+      description = "Accept systemd time format like 'hourly', 'daily', '*:0/30'. Leave empty to disable";
+    };
+  };
+
+  config = lib.mkIf cfg.enable
+  {
+    systemd.tmpfiles.rules =
+    [
+      "d ${configDir} 0755 mihomo mihomo -"
+    ];
+
+    systemd.services.mihox-prepare =
+    {
+      description = "Prepare initial mihomo config";
+      before = [ "mihomo.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig =
+      {
+        Type = "oneshot";
+        User = "mihomo";
+        ExecStart =
+        "
+          ${pkgs.curl}/bin/curl -Lo ${configFile}.tmp ${cfg.url} \
+          && mv ${configFile}.tmp ${configFile} \
+          || true
+        ";
+      };
+    };
+
+    systemd.services.mihox-fetcher =
+    {
+      description = "Fetch mihomo subscription";
+      serviceConfig =
+      {
+        Type = "oneshot";
+        User = "mihomo";
+        ExecStart =
+        "
+          ${pkgs.curl}/bin/curl -Lo ${configFile}.tmp ${cfg.url} \
+          && mv ${configFile}.tmp ${configFile}
+        ";
+        ExecStartPost = "${pkgs.systemd}/bin/systemctl try-restart mihomo.service";
+      };
+    };
+
+    systemd.timers.mihoz-fetcher = lib.mkIf (cfg.interval != "")
+    {
+      description = "Timer for mihomo fetcher";
+      wantedBy = [ "timers.target" ];
+      timerConfig =
+      {
+        OnCalendar = cfg.interval;
+        Persistent = true;
+      };
+    };
+  };
+}
+CFH
 
 cat <<CFK> "$CONF_KIX"
 { config, pkgs, ... }:
@@ -43,9 +128,6 @@ cat <<CFK> "$CONF_KIX"
   #
   # Kitsunori Customization
   #
-
-  # Nix Channel
-  #nix.nixPath = [ "nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-unstable-small.tar.gz" ];
   
   # Features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -82,7 +164,31 @@ cat <<CFK> "$CONF_KIX"
   virtualisation.docker.enable = true;
   virtualisation.docker.rootless.enable = true;
   virtualisation.docker.rootless.setSocketVariable = true;
-}
 CFK
+
+if curl  -sfIL "https://$MHX_URL" &>/dev/null; then
+  echo "Mihomo subscription found."
+  cat <<CFKH>> "$CONF_KIX"
+  services.mihox =
+  {
+    enable = true;
+    url = "$MHX_URL";
+    #interval = "weekly";
+  };
+
+  services.mihomo = 
+  {
+    enable = true;
+    configFile = "/var/lib/mihomo/config.yaml";
+    tunMode = true;
+  };
+}
+CFKH
+else
+  cat <<CFKH>> "$CONF_KIX"
+}
+CFKH
+fi
+
 nix-channel --add https://channels.nixos.org/nixos-unstable-small nixos
 nixos-rebuild switch --upgrade
