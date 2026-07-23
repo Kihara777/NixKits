075d969ef93e45031974aced718ffb659be83053
Kitsunori <kitsunori@outlook.jp>
2026-07-23 10:57:29 +0900

    fix(overlays): remove { }: from breeze-black.nix

--- overlays/breeze-black.nix
+++ overlays/breeze-black.nix
@@ -1,22 +1,4 @@
-{ }:
-(final: prev: let
-  breezeBlackColors = ./breeze-black.colors;
-in {
-  kdePackages = prev.kdePackages // {
-    breeze = prev.kdePackages.breeze.overrideAttrs (old: {
-      patches = (old.patches or [ ]) ++ [
-        (prev.fetchpatch {
-          name = "breeze-black.patch";
-          url = "https://injx.sbs/breeze-black/breeze-black.patch";
-          hash = "sha256-SV4xkqq9nK7lCXnBI59uGB3XwKp4umq5t2v63IbKYog=";
-        })
-      ];
-      postPatch = (old.postPatch or "") + ''
-        cp ${breezeBlackColors} $out/share/color-schemes/BreezeBlack.colors
-      '';
-    });
-
-    breeze-gtk = prev.kdePackages.breeze-gtk.overrideAttrs (old: {
+(final: prev: let    breeze-gtk = prev.kdePackages.breeze-gtk.overrideAttrs (old: {
       postPatch = (old.postPatch or "") + ''
         substituteInPlace CMakeLists.txt \
           --replace-fail 'assets' 'assets-dark'
